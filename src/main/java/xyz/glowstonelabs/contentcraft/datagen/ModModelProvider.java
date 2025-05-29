a7d7fd724b82cb170e6a728719e7103585605e8a
Hakerboi <racerburhan@gmail.com>
2025-05-29 21:57:04 +0100

    ported over features from old mod

---
+++ src/main/java/xyz/glowstonelabs/contentcraft/datagen/ModModelProvider.java
@@ -0,0 +1,50 @@
+package xyz.glowstonelabs.contentcraft.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+import net.minecraft.data.client.TexturedModel;
+import net.minecraft.item.ArmorItem;
+import xyz.glowstonelabs.contentcraft.init.ModBlocks;
+import xyz.glowstonelabs.contentcraft.init.ModItems;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.XAENON_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RAW_XAENON_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.XAENON_ORE);
+
+        blockStateModelGenerator.registerLog(ModBlocks.MAPLE_LOG).log(ModBlocks.MAPLE_LOG).wood(ModBlocks.MAPLE_WOOD);
+        blockStateModelGenerator.registerLog(ModBlocks.STRIPPED_MAPLE_LOG).log(ModBlocks.STRIPPED_MAPLE_LOG).wood(ModBlocks.STRIPPED_MAPLE_WOOD);
+
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.MAPLE_PLANKS);
+        blockStateModelGenerator.registerSingleton(ModBlocks.MAPLE_LEAVES, TexturedModel.LEAVES);
+        blockStateModelGenerator.registerTintableCrossBlockState(ModBlocks.MAPLE_SAPLING, BlockStateModelGenerator.TintType.NOT_TINTED);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.XAENON_INGOT, Models.GENERATED);
+        itemModelGenerator.register(ModItems.RAW_XAENON, Models.GENERATED);
+
+
+        itemModelGenerator.register(ModItems.XAENON_SWORD, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.XAENON_PICKAXE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.XAENON_SHOVEL, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.XAENON_AXE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.XAENON_HOE, Models.HANDHELD);
+
+
+        itemModelGenerator.registerArmor(((ArmorItem) ModItems.XAENON_HELMET));
+        itemModelGenerator.registerArmor(((ArmorItem) ModItems.XAENON_CHESTPLATE));
+        itemModelGenerator.registerArmor(((ArmorItem) ModItems.XAENON_LEGGINGS));
+        itemModelGenerator.registerArmor(((ArmorItem) ModItems.XAENON_BOOTS));
+    }
+}
